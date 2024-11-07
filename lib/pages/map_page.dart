be7b0cf34cfbf1a00903c0b7d8bc6502defd277c
Han Min Thaw <hanminthaw15@gmail.com>
2024-11-07 17:57:04 +0700

    added location package

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 import 'package:mfu_guide/user_location.dart';
 
 class MapPage extends StatefulWidget {
@@ -10,29 +11,113 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  UserLocation userLoc = UserLocation();
+  Location _locationController = new Location();
+  static const LatLng _pMsquare = LatLng(20.046588486327085, 99.89128965622983);
+  static const LatLng _pD1Canteen =
+      LatLng(20.047726253086168, 99.89377030087009);
+  LatLng? _currentP = null;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocationUpdates();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Map Guide'),
       ),
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: userLoc.pMsquare,
-          zoom: 16,
-        ),
-        markers: {
-          Marker(
-              markerId: MarkerId("_currentLocation"),
-              icon: BitmapDescriptor.defaultMarker,
-              position: userLoc.pMsquare),
-          Marker(
-              markerId: MarkerId("_sourceLocation"),
-              icon: BitmapDescriptor.defaultMarker,
-              position: userLoc.pD1Canteen),
-        },
-      ),
+      body: _currentP == null
+          ? Center(child: Text('Loading...'))
+          : GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: _pMsquare,
+                zoom: 16,
+              ),
+              markers: {
+                Marker(
+                    markerId: MarkerId("_currentLocation"),
+                    icon: BitmapDescriptor.defaultMarker,
+                    position: _pMsquare),
+                Marker(
+                    markerId: MarkerId("_sourceLocation"),
+                    icon: BitmapDescriptor.defaultMarker,
+                    position: _pD1Canteen),
+              },
+            ),
     );
+  }
+
+  // Future<void> getLocationUpdates() async {
+  //   bool _serviceEnabled;
+  //   PermissionStatus _permissionGranted;
+  //   _serviceEnabled = await _locationController.serviceEnabled();
+  //   if (_serviceEnabled) {
+  //     _serviceEnabled = await _locationController.requestService();
+  //   } else {
+  //     return;
+  //   }
+  //   _permissionGranted = await _locationController.hasPermission();
+  //   if (_permissionGranted == PermissionStatus.denied) {
+  //     _permissionGranted = await _locationController.requestPermission();
+  //     if (_permissionGranted != PermissionStatus.granted) {
+  //       return;
+  //     }
+  //   }
+  //   _locationController.onLocationChanged
+  //       .listen((LocationData currentLocation) {
+  //     if (currentLocation.latitude != null &&
+  //         currentLocation.longitude != null) {
+  //       setState(() {
+  //         _currentP =
+  //             LatLng(currentLocation.latitude!, currentLocation.longitude!);
+  //         print(_currentP);
+  //       });
+  //     }
+  //   });
+  // }
+
+  Future<void> getLocationUpdates() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    // Check if location services are enabled
+    _serviceEnabled = await _locationController.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await _locationController.requestService();
+      if (!_serviceEnabled) {
+        print('Location services are not enabled');
+        return;
+      }
+    }
+
+    // Check if permission is granted
+    _permissionGranted = await _locationController.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _locationController.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        print('Location permission denied');
+        return;
+      }
+    }
+
+    // Start listening to location changes
+    _locationController.onLocationChanged
+        .listen((LocationData currentLocation) {
+      if (currentLocation.latitude != null &&
+          currentLocation.longitude != null) {
+        print(
+            'Current location: ${currentLocation.latitude}, ${currentLocation.longitude}');
+        setState(() {
+          _currentP =
+              LatLng(currentLocation.latitude!, currentLocation.longitude!);
+        });
+      } else {
+        print('Location data is null');
+      }
+    });
   }
 }
--- lib/user_location.dart
+++ lib/user_location.dart
@@ -1,6 +1,5 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'package:flutter/material.dart';
 
-class UserLocation {
-  LatLng pMsquare = LatLng(20.046588486327085, 99.89128965622983);
-  LatLng pD1Canteen = LatLng(20.047726253086168, 99.89377030087009);
-}
+class UserLocation {}
