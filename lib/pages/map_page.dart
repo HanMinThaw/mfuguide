930900ae94f64cbe55085c1693af78121bf1417b
Han Min Thaw <hanminthaw15@gmail.com>
2024-11-07 19:34:29 +0700

    complete basic map

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
-import 'package:mfu_guide/user_location.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:mfu_guide/const.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -12,16 +14,23 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   Location _locationController = new Location();
+  final Completer<GoogleMapController> _mapController =
+      Completer<GoogleMapController>();
   static const LatLng _pMsquare = LatLng(20.046588486327085, 99.89128965622983);
   static const LatLng _pD1Canteen =
       LatLng(20.047726253086168, 99.89377030087009);
   LatLng? _currentP = null;
+  Map<PolylineId, Polyline> polylines = {};
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getLocationUpdates();
+    getLocationUpdates().then((_) => {
+          getPolyLinePoints().then((coordinates) => {
+                generatePolyLineFromPoints(coordinates),
+              })
+        });
   }
 
   @override
@@ -35,18 +44,25 @@ class _MapPageState extends State<MapPage> {
           : GoogleMap(
               initialCameraPosition: CameraPosition(
                 target: _pMsquare,
-                zoom: 16,
+                zoom: 13,
               ),
+              onMapCreated: ((GoogleMapController controller) =>
+                  _mapController.complete(controller)),
               markers: {
                 Marker(
                     markerId: MarkerId("_currentLocation"),
                     icon: BitmapDescriptor.defaultMarker,
-                    position: _pMsquare),
+                    position: _currentP!),
                 Marker(
                     markerId: MarkerId("_sourceLocation"),
                     icon: BitmapDescriptor.defaultMarker,
+                    position: _pMsquare),
+                Marker(
+                    markerId: MarkerId("_destinationLocation"),
+                    icon: BitmapDescriptor.defaultMarker,
                     position: _pD1Canteen),
               },
+              polylines: Set<Polyline>.of(polylines.values),
             ),
     );
   }
@@ -79,6 +95,53 @@ class _MapPageState extends State<MapPage> {
   //     }
   //   });
   // }
+
+  // Future<void> getLocationUpdates() async {
+  //   bool _serviceEnabled;
+  //   PermissionStatus _permissionGranted;
+  //
+  //   // Check if location services are enabled
+  //   _serviceEnabled = await _locationController.serviceEnabled();
+  //   if (!_serviceEnabled) {
+  //     _serviceEnabled = await _locationController.requestService();
+  //     if (!_serviceEnabled) {
+  //       print('Location services are not enabled');
+  //       return;
+  //     }
+  //   }
+  //
+  //   // Check if permission is granted
+  //   _permissionGranted = await _locationController.hasPermission();
+  //   if (_permissionGranted == PermissionStatus.denied) {
+  //     _permissionGranted = await _locationController.requestPermission();
+  //     if (_permissionGranted != PermissionStatus.granted) {
+  //       print('Location permission denied');
+  //       return;
+  //     }
+  //   }
+  //
+  //   // Start listening to location changes
+  //   _locationController.onLocationChanged
+  //       .listen((LocationData currentLocation) {
+  //     if (currentLocation.latitude != null &&
+  //         currentLocation.longitude != null) {
+  //       print(
+  //           'Current location: ${currentLocation.latitude}, ${currentLocation.longitude}');
+  //       setState(() {
+  //         _currentP =
+  //             LatLng(currentLocation.latitude!, currentLocation.longitude!);
+  //       });
+  //     } else {
+  //       print('Location data is null');
+  //     }
+  //   });
+  // }
+  Future<void> _cameraToPosition(LatLng pos) async {
+    final GoogleMapController controller = await _mapController.future;
+    CameraPosition _newCameraPosition = CameraPosition(target: pos, zoom: 17);
+    await controller
+        .animateCamera(CameraUpdate.newCameraPosition(_newCameraPosition));
+  }
 
   Future<void> getLocationUpdates() async {
     bool _serviceEnabled;
@@ -114,10 +177,44 @@ class _MapPageState extends State<MapPage> {
         setState(() {
           _currentP =
               LatLng(currentLocation.latitude!, currentLocation.longitude!);
+          _cameraToPosition(_currentP!);
+          print('Updated _currentP: $_currentP'); // Debugging line
         });
       } else {
         print('Location data is null');
       }
+    });
+  }
+
+  Future<List<LatLng>> getPolyLinePoints() async {
+    List<LatLng> polylineCoordinates = [];
+    PolylinePoints polylinePoints = PolylinePoints();
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        googleApiKey: Google_Maps_API_Key,
+        request: PolylineRequest(
+            origin: PointLatLng(_pMsquare.latitude, _pMsquare.longitude),
+            destination:
+                PointLatLng(_pD1Canteen.latitude, _pD1Canteen.longitude),
+            mode: TravelMode.walking));
+    if (result.points.isNotEmpty) {
+      result.points.forEach((PointLatLng point) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      });
+    } else {
+      print(result.errorMessage);
+    }
+    return polylineCoordinates;
+  }
+
+  void generatePolyLineFromPoints(List<LatLng> polylineCoordinates) async {
+    PolylineId id = PolylineId("poly");
+    Polyline polyline = Polyline(
+        polylineId: id,
+        color: Colors.black,
+        points: polylineCoordinates,
+        width: 8);
+    setState(() {
+      polylines[id] = polyline;
     });
   }
 }
